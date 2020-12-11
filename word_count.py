27913269729685988e79edf8192d075f0acb24d5
ba-marwa <71134546+ba-marwa@users.noreply.github.com>
2020-12-11 21:58:18 +0100

    Add files via upload

---
+++ word_count.py
@@ -0,0 +1,18 @@
+from pyspark import SparkContext, SparkConf
+ 
+conf =SparkConf().setAppName("wordCount").setMaster("local")
+sc = SparkContext(conf=conf)
+
+	
+Fichier=sc.textFile("C:/Users/sakur/OneDrive/Documents/Cours/Outils Data Mining/Test word count/sample.txt")
+
+# read data from text file and split each line into words
+words = Fichier.flatMap(lambda line: line.split(" "))
+	
+# count the occurrence of each word
+wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
+
+print(count.collect())
+	
+# save the counts to output
+wordCounts.saveAsTextFile("C:/Users/sakur/OneDrive/Documents/Cours/Outils Data Mining/Test word count/")
